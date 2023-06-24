6027320ad063af1d8c5a7c1d115f3d9be87fc843
PingxuHao <pvh5251@psu.edu>
2023-06-23 19:36:48 -0700

    1

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ app.layout = dcc.Loading(  # <- Wrap App with Loading Component
 server = app.server
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
